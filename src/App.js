61e956d8c53f572c6d577636d2d9a21bb50f1fa2
Justin Peisker <justinpeisker@gmail.com>
2021-10-08 10:46:50 -0700

    progress

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
+import Char from './components/Character';
+
+
 const App = () => {
-  const [characters, setCharacters] = useState([]);
+  const [charList, setCharList] = useState([]);
 
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -14,9 +17,9 @@ const App = () => {
   useEffect(() => {
     axios.get('https://swapi.dev/api/people')
       .then(res => {
-        setCharacters(res.data)
+        setCharList(res.data)
         console.log(res);
-      }).catch(err =>{
+      }).catch(err => {
         console.error(err);
       })
     
@@ -25,7 +28,10 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <h3>{charList[0].name}</h3>
+      <Char char={charList} />
     </div>
+   
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,4 +1,26 @@
 // Write your Character component here
 import React, {useState, useEffect} from 'react';
-import './App.css';
-import axios from 'axios';
+// import axios from 'axios';
+
+
+export default function Char(props) {
+    const [char, setChar] = useState([]);
+    // const {chars} = props;
+
+    return(
+        <div className= 'char-container'>
+            <div className= 'char-info'>
+                {/* <h3>{char[0].name}</h3> 
+                <p>{char[0].gender}</p> 
+                <p>{char[0].height}</p>
+                <p>{char[0].mass}</p>
+                <p>{char[0].birth_year}</p>
+                <p>{char[0].eye_color}</p>
+                <p>{char[0].hair_color}</p>
+                <p>{char[0].skin_color}</p> */}
+            </div>
+
+        </div>
+    )
+}
+
